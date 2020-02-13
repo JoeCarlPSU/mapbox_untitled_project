d6998c6cf3b82fd72a5b57a6ee7e9e139cce1109
JoeCarlPSU <joescottcarl@gmail.com>
2020-02-13 12:58:35 -0500

    initial geocortext style set-up

--- js/js.js
+++ js/js.js
@@ -1,8 +1,23 @@
+let map;
 $(document).ready(() => {
-    var map = new mapboxgl.Map({
-        container: 'map', // container id
-        style: 'https://maputnik.github.io/osm-liberty/style.json',
-        center: [-74.5, 40], // starting position [lng, lat]
-        zoom: 9 // starting zoom
-        });
+    map = new ol.Map({
+        target: 'map',
+        layers: [
+          new ol.layer.Tile({
+            source: new ol.source.XYZ({
+              attributions: ['Powered by Esri',
+                             'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
+              attributionsCollapsible: true,
+              url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+              maxZoom: 23
+            })
+          })
+        ],
+        view: new ol.View({
+          projection: 'EPSG:4326',
+          // center: ol.proj.fromLonLat([37.41, 8.82]),
+          center: [-97.62702097624204, 37.809133887525924],
+          zoom: 15
+        })
+      });
 })
